d1a0081309868c4171067780e3cf85bbf7660a2b
chefdude99 <chefdude.1999@gmail.com>
2024-10-10 19:28:17 +0000

    Landing page, listo

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -3,11 +3,9 @@ import React from "react"
 const Jumbotron = () => {
     return (
         <div className="jumbotron container bg-light my-4 p-2">
-            <h1 className="display-4">Hello, world!</h1>
+            <h1 className="display-4">A WARM WELCOME!</h1>
             <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr className="my-4" />
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
         </div>
     )
 }
